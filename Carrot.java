99aaf0059f61fda640156de6f656170ea03e4cd5
GayathriBalakumaran <77260500+GayathriBalakumaran@users.noreply.github.com>
2021-01-22 20:24:16 +0530

    Add files via upload

---
+++ Carrot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Carrot{
+    public static void main(String []args){
+        Scanner kbd=new Scanner(System.in);
+        int n1,n2;
+        String w1,w2;
+        n1=kbd.nextInt();
+        n2=kbd.nextInt();
+        w1=kbd.nextLine();
+        w2=kbd.nextLine();
+        System.out.print(n2);
+    }
+}
---
+++ carrots.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    string w1,w2;
+    cin>>n1>>n2;
+    cin>>w1>>w2;
+    cout<<n2;
+    return 0;
+}
